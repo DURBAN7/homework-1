d4bbe6b1ced2dd7c46e1586897095eac4410b023
DURBAN7 <162716506+DURBAN7@users.noreply.github.com>
2024-03-08 19:27:13 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,280 @@
+// let userName = 'Kurwa';
+// const myBirthday = '07.12.2008';
+
+//  alert(userName);
+// let num = +prompt('Введите любое число');
+
+// for (let i = 0; i < num; i++) {
+//   console.000000000000000000000000000000 log('#');
+// }
+
+// number
+// 77777777;
+
+// string
+// ('blablablabla');
+
+// biging
+// 88888888888888n;
+
+// null - ничего, пусто
+// let age = null;
+
+// undefind - озночает что значение не было присвоено
+// let name;
+// console.log(name);
+
+// object - используется для хранения коллекций данных.
+// let user = {
+//   name: 'fakanamuna',
+//   age: 16,
+//   job: 'pupil',
+//   height: 175,
+// };
+
+// console.log(user.name, user.job, user.height);
+
+//symbol - для уникальных идентификаторов
+
+//boolean - true(да) false(нет)
+//let num = 2111211;
+//console.log(typeof num);
+
+// let isBoss = confirm('Улугбек гей ?')
+//    console.log(isBoss)
+
+// let num1 = 77;
+
+// console.log(--num1);
+
+// console.log(2 > 1); //true
+// console.log(2 === 1); //false
+// console.log(2 >= 2);
+
+// let num1 = 10;
+// let num2 = 20;
+
+// if(num1 === 10 && num2 === 20) {
+//    console.log(true);
+// } else {
+//    console.log(false);
+// }
+
+// let year = +prompt('В каком году вы родились ? ');
+
+// if(year < 2008) {
+//    console.log('Это слишком поздно!');
+// } else if (year > 2008){
+//    console.log('Это поздно !');
+// } else {
+//    console.log('Верно');
+// }
+//  let minNum = +prompt();
+//  let maxNum = +prompt();
+
+//   for (let i = minNum; i < maxNum; i = i + 4) {
+//    console.log(i);
+//   }
+//   let num = +prompt('Введите любое число');
+
+// if(num > 0) {
+// console.log('+');
+// } else if (num < 0) {
+// console.log('-');
+// } else {
+// console.log('=');
+// }
+
+// let userName = prompt('')
+// const myBirthday = '07.12.2008';
+
+// 1.
+// let string = 'Anita MAX WYNN'
+
+// 2.
+// let num = 20;
+
+// 3.bigint для целых чисел произвольной длины.
+// const bigInt = 4525898653259632415263201471784n;
+
+// 4. true or false - boolean
+// let nameFieldChecked = true;
+// let ageFieldChecked = false;
+
+// 5.
+// let age = null;
+
+// 6.
+// let user = {
+// name: 'Anita MAX WYNN',
+// age: 16,
+// job: 'Pupil',
+// money: 300000-som,
+// };
+// 7.symbol
+// let
+
+// console.log(typeof 40);
+
+// let name = 'ULUG Suchka';
+// name = 'ULUG suchka';
+// console.log(name);
+
+// confirm();
+
+// console.log(!(10 == 10));
+
+// let num;
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// function showMassage() {
+//   let massage = 'Ulug гнида!';
+
+//   console.log(massage);
+// }
+
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+// showMassage();
+
+// function showMassage(from, text) {
+
+//   console.log('$(from): $(text)');
+// }
+
+// showMassage('ULUg', 'gay');
+
+// function getSum(a, b) {
+//   console.log(a+b);
+// }
+
+// getSum(5, 975);
+// getSum(5, 95);
+// getSum(5, 85);
+// getSum(5, 45);
+// getSum(5, 55);
+
+// function checkNumber(num1, num2) {
+//   if (num1 < num2) {
+//     console.log(-1);
+//   } else if (num1 > num2) {
+//     console.log(1);
+//   } else {
+//     console.log(0);
+//   }
+// }
+
+// checkNumber(777, 100);
+// checkNumber(7, 10);
+// checkNumber(77, 10);
+
+//  task 2
+
+// function getSum(a){
+// console.log();
+
+// }
+
+// console.log(typeof'str');
+
+// if(num < 16) {
+//   console.log();
+// }
+// else if (num > 16) {
+//   console.log(num);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// function showMassage(name, text) {
+//   console.log(`${name}: ${text}`);
+// }
+
+// showMassage('Durbek','Ulug');
+
+// 2
+
+// let showMassage = function (num1, num2) {
+//   console.log(num1 + num2);
+// };
+
+// showMassage(16   , 18);
+
+function calc(num1, num2, math) {
+  if (math === '+') {
+    console.log(num1 + num2);
+  } else if (math === '-') {
+    console.log(num1 - num2);
+  } else if (math === '*') {
+    console.log(num1 * num2);
+  } else if (math === '/') {
+    console.log(num1 / num2);
+  } else {
+    console.log('Ошибка');
+  }
+}
+
+calc(18, 17, '*');
